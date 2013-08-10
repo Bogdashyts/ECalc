7052a4ea2e1745895d30c1e46c2216182bafab9d
bpgdashyts <Vaniych@tut.by>
2013-08-10 18:41:55 +0300

    hh

--- ECalculator/Controllers/CalcController.cs
+++ ECalculator/Controllers/CalcController.cs
@@ -7,7 +7,7 @@ using ECalculator.Models;
 
 namespace ECalculator.Controllers
 {
-    public class CalcController : Controllern
+    public class CalcController : Controhllern
     {
         //
         // GET: /Calc/
